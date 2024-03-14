2fe765460f0a62e3509f8e17e74c323fd8eeb27b
AhmadAlkaseb <ahmadalkasebpri@icloud.com>
2024-03-14 12:13:51 +0100

    .

---
+++ src/main/java/ThirdSemesterExercises/Backend/Week11Year2024/FridayReview/Review.java
@@ -0,0 +1,33 @@
+package ThirdSemesterExercises.Backend.Week11Year2024.FridayReview;
+
+
+// Demonstrate how to use a variety of Hamcrest matchers to write expressive assertions in Rest Assured tests.
+public class Review {
+    /*
+    @Test
+    public void testGetRequest() {
+
+                 RestAssured.get("http://localhost:7007/api/test")
+                .then()
+                .statusCode(200) // Tjek at det hele gik godt
+
+
+
+                .body("name", equalTo("John")) // Tjek om navnet findes
+
+                .body("age", greaterThan(18)) // Tjek om alderen er højere end et tal
+
+                .body("email", containsString("@example.com")) // Tjek at email indeholder en tekst
+
+                .body("address.city", notNullValue()) // Tjek at byen ikke null
+
+                .body("hobbies", hasSize(3)) // Tjek at en liste af hobbyer er 3
+
+                .body("hobbies", hasItem("reading")) // Tjek at listen indeholder en entity med navnet "reading".
+
+                .body("hobbies", everyItem(not(emptyString()))) // Tjek at hver entity i listen ikke er null
+
+                .body("adult", is(true)); // Tjek at personen er en voksen
+    }
+   */
+}
