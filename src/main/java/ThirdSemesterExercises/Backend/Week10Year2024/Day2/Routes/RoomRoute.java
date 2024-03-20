3329d4271edab2a6b2ba437edc322c1266f09939
AhmadAlkaseb <ahmadalkasebpri@icloud.com>
2024-03-20 14:42:07 +0100

    .

--- src/main/java/ThirdSemesterExercises/Backend/Week10Year2024/Day2/Routes/RoomRoute.java
+++ src/main/java/ThirdSemesterExercises/Backend/Week10Year2024/Day2/Routes/RoomRoute.java
@@ -1,8 +1,11 @@
 package ThirdSemesterExercises.Backend.Week10Year2024.Day2.Routes;
 
 import ThirdSemesterExercises.Backend.Week10Year2024.Day2.Controllers.RoomController;
+import ThirdSemesterExercises.Backend.Week10Year2024.Day2.Controllers.SecurityController;
 import ThirdSemesterExercises.Backend.Week10Year2024.Day2.DAOs.RoomDAO;
+import ThirdSemesterExercises.Backend.Week10Year2024.Day2.Persistence.HibernateConfig;
 import io.javalin.apibuilder.EndpointGroup;
+import jakarta.persistence.EntityManagerFactory;
 
 import static io.javalin.apibuilder.ApiBuilder.*;
 
@@ -13,8 +16,12 @@ public class RoomRoute {
         this.roomDAO = roomDAO;
     }
 
+    private static EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryConfig(false);
+    private static SecurityController securityController = new SecurityController(emf);
+
     public EndpointGroup roomRoutes() {
         return () -> {
+            before(securityController.authenticate());
             get("/rooms", RoomController.getAll(roomDAO), Role.ADMIN);
             get("/rooms/{id}", RoomController.getRoom(roomDAO), Role.ADMIN);
             post("/rooms", RoomController.create(roomDAO), Role.ADMIN);
--- src/test/java/ThirdSemesterExercises/Backend/Week10Year2024/Day2/AppRestTest.java
+++ src/test/java/ThirdSemesterExercises/Backend/Week10Year2024/Day2/AppRestTest.java
@@ -14,15 +14,11 @@ import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
@@ -50,6 +46,18 @@ class AppRestTest {
                 .setExceptionHandlers()
                 .checkSecurityRoles()
                 .setRoute(route.addRoutes());
+    }
+
+    @AfterAll
+    static void tearDown() {
+        emf.close();
+        app.stopServer();
+    }
+
+    @BeforeEach
+    void beforeEach(){
+
+        //Lav objekter
 
         Hotel hotel1 = new Hotel("Hotel A", "Lyngby vej");
         Hotel hotel2 = new Hotel("Hotel B", "Roskilde vej");
@@ -114,10 +122,73 @@ class AppRestTest {
         adminUsername = JsonPath.from(login).getString("username");
     }
 
-    @AfterAll
-    static void tearDown() {
-        emf.close();
-        app.stopServer();
+    @AfterEach
+    void AfterEach(){
+
+        //Tøm databasen
+
+        //fx
+        try(EntityManager em = emf.createEntityManager()){
+            em.getTransaction().begin();
+            em.createQuery("DELETE FROM Room").executeUpdate();
+            em.createQuery("DELETE FROM Hotel").executeUpdate();
+            em.createQuery("DELETE FROM User").executeUpdate();
+            em.createQuery("DELETE FROM Role").executeUpdate();
+            em.getTransaction().commit();
+        }
+
+    }
+    @Test
+    @DisplayName("Testing register user and token retrieval")
+    void getUserRoutes() {
+
+        String setBody = "{\"username\": \"UserTest2\", \"password\": \"1234\"}";
+
+        String response =
+                RestAssured
+                        .given()
+                        .contentType(ContentType.JSON)
+                        .body(setBody)
+                        .when()
+                        .post("/auth/register")
+                        .then()
+                        .statusCode(201)
+                        .extract().asString();
+
+
+        String token = JsonPath.from(response).getString("token");
+        String username = JsonPath.from(response).getString("username");
+        System.out.println("Response: "+response + "\nToken: " + token +"\nUsername: "+username);
+    }
+
+    @Test
+    @DisplayName("Login and retrieve token")
+    void test0() {
+
+        String setRegisterBody = "{\"username\": \"UserTest3\", \"password\": \"1234\"}";
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body(setRegisterBody)
+                .when()
+                .post("/auth/register")
+                .then();
+
+        String setLoginBody = "{\"username\": \"UserTest3\", \"password\": \"1234\"}";
+
+        String response = RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body(setLoginBody)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(200)
+                .extract().asString();
+
+        String token = JsonPath.from(response).getString("token");
+        String username = JsonPath.from(response).getString("username");
+        System.out.println("Response: " + response + "\nToken: " + token + "\nUsername: " + username);
     }
 
     @Test
@@ -376,5 +447,43 @@ class AppRestTest {
                 .body("price", equalTo(500))
                 .body("hotelId", equalTo(expectedHotelId))
                 .body("id", equalTo(expectedHotelId));
+    }
+
+    @Test
+    @DisplayName("Access specific route via token user-role")
+    void test(){
+
+        String setUserName = "setUserName";
+        String setUserPassword = "1234";
+
+        String setBody = "{\"username\": \""+setUserName +"\", \"password\": \""+setUserPassword+"\"}";
+
+        String response =
+                RestAssured
+                        .given()
+                        .contentType(ContentType.JSON)
+                        .body(setBody)
+                        .when()
+                        .post("/auth/register")
+                        .then()
+                        .statusCode(201)
+                        .body("token", notNullValue())
+                        .body("username", equalTo(setUserName))
+                        .extract().asString();
+
+
+        String token = JsonPath.from(response).getString("token");
+
+        String expectedString = "Hello from USER Protected";
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer " + token)
+                //.body(setBody) -> da vi ikke skal benytte POST sender vi ikke en body med
+                .when()
+                .get("/protected/user_demo")
+                .then()
+                .statusCode(200)
+                .body("msg", equalTo(expectedString));
     }
 }
