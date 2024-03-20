f97beb62d7c2943d6cb1ef956e767c3b8a0dde86
AhmadAlkaseb <ahmadalkasebpri@icloud.com>
2024-03-20 15:39:55 +0100

    .

---
+++ src/main/java/ThirdSemesterExercises/Backend/Week12Year2024/Day2/Exercise2/TDDExercise.java
@@ -0,0 +1,9 @@
+package ThirdSemesterExercises.Backend.Week12Year2024.Day2.Exercise2;
+
+public class TDDExercise {
+
+}
+
+// Link til testen
+
+// 
---
+++ src/test/java/ThirdSemesterExercises/Backend/Week12Year2024/Day2/Exercise2/TDDExerciseTest.java
@@ -0,0 +1,7 @@
+package ThirdSemesterExercises.Backend.Week12Year2024.Day2.Exercise2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TDDExerciseTest {
+
+}
